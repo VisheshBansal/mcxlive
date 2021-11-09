a6b78a7d3e0e4974c2ab65208e085c69f1015d44
VisheshBansal <vishesh.bansal@gmail.com>
2021-11-10 00:37:02 +0530

    API Call working

--- lib/models/stock.dart
+++ lib/models/stock.dart
@@ -15,36 +15,32 @@ final Map<String, String> tokenData = {
   'If-None-Match': 'W/"35f6-lbZAd6vCBUugPaqRPtDi0w"'
 };
 
-const jsonData =
-    '[{"Exchange":"MCX","Symbol":"GOLD","Ser/Exp":"03DEC2021","Last Traded Price":"48209","Buy Price":"48209","Sell Price":"48217","High":"48270","Low":"47976","Open":"48015","Close":"48018","Last Update Time":"07:59:10 PM 09/11/2021","Net Change In Rs":"191","TBQ":"525","TSQ":"396","OI":"7625","% Net Change In Rs":"0.40","DPR":"46578 - 49458","Vol":"4.435","ATP":"48117.20","Code":"226469"},{"Exchange":"MCX","Symbol":"GOLDM","Ser/Exp":"03DEC2021","Last Traded Price":"48199","Buy Price":"48190","Sell Price":"48199","High":"48245","Low":"47981","Open":"48034","Close":"48025","Last Update Time":"07:59:10 PM 09/11/2021","Net Change In Rs":"174","TBQ":"1617","TSQ":"1405","OI":"15675","% Net Change In Rs":"0.36","DPR":"46585 - 49465","Vol":"9.128","ATP":"48126.10","Code":"233000"},{"Exchange":"MCX","Symbol":"SILVER","Ser/Exp":"03DEC2021","Last Traded Price":"64922","Buy Price":"64904","Sell Price":"64918","High":"65080","Low":"64643","Open":"64877","Close":"64881","Last Update Time":"07:59:11 PM 09/11/2021","Net Change In Rs":"41","TBQ":"486","TSQ":"536","OI":"9536","% Net Change In Rs":"0.06","DPR":"62286 - 67476","Vol":"6.340","ATP":"64895.62","Code":"226471"},{"Exchange":"MCX","Symbol":"SILVERM","Ser/Exp":"30NOV2021","Last Traded Price":"65096","Buy Price":"65085","Sell Price":"65094","High":"65240","Low":"64831","Open":"65010","Close":"65027","Last Update Time":"07:59:11 PM 09/11/2021","Net Change In Rs":"69","TBQ":"1543","TSQ":"2520","OI":"20898","% Net Change In Rs":"0.11","DPR":"62426 - 67628","Vol":"20.950","ATP":"65070.36","Code":"225315"},{"Exchange":"MCX","Symbol":"CRUDEOIL","Ser/Exp":"18NOV2021","Last Traded Price":"6120","Buy Price":"6120","Sell Price":"6121","High":"6136","Low":"6053","Open":"6053","Close":"6058","Last Update Time":"07:59:11 PM 09/11/2021","Net Change In Rs":"62","TBQ":"1494","TSQ":"795","OI":"6478","% Net Change In Rs":"1.02","DPR":"5816 - 6300","Vol":"28.025","ATP":"6096.31","Code":"230024"},{"Exchange":"MCX","Symbol":"COPPER","Ser/Exp":"30NOV2021","Last Traded Price":"737.90","Buy Price":"737.90","Sell Price":"738.00","High":"746.00","Low":"734.35","Open":"737.90","Close":"739.40","Last Update Time":"07:59:10 PM 09/11/2021","Net Change In Rs":"-1.50","TBQ":"467","TSQ":"686","OI":"4955","% Net Change In Rs":"-0.20","DPR":"709.85 - 768.95","Vol":"9.327","ATP":"740.01","Code":"230802"},{"Exchange":"MCX","Symbol":"NICKEL","Ser/Exp":"30NOV2021","Last Traded Price":"1506.60","Buy Price":"1506.00","Sell Price":"1506.80","High":"1519.80","Low":"1502.00","Open":"1508.60","Close":"1508.90","Last Update Time":"07:59:10 PM 09/11/2021","Net Change In Rs":"-2.30","TBQ":"263","TSQ":"332","OI":"1440","% Net Change In Rs":"-0.15","DPR":"1448.60 - 1569.20","Vol":"4.057","ATP":"1510.38","Code":"230822"}]';
-
-// ignore: non_constant_identifier_names
-Map<String, dynamic> raw_data = json.decode(jsonData);
 
 Future<List<Stock>> getData(String data) async {
   List<Stock> list;
-  final String apiEndpoint = 'http://88.99.61.159:5050';
+  final String apiEndpoint = 'http://88.99.61.159:5050/getdata';
   final Uri url = Uri.parse(apiEndpoint);
   var res = await http.get(url, headers: tokenData);
-  print(res.body);
   if (res.statusCode == 200) {
     var data = json.decode(res.body);
     var rest = data["data"] as List;
-    print(rest);
     list = rest.map<Stock>((json) => Stock.fromJson(json)).toList();
+  } else {
+    throw ("Error: ${res.statusCode}");
   }
-  return list;
+  print(list);
+  return (list);
 }
 
 class Stock {
   final String symbol;
   final String expdate;
-  final double ltp;
-  final double high;
-  final double low;
-  final double open;
-  final double close;
-  final double change;
+  final String ltp;
+  final String high;
+  final String low;
+  final String open;
+  final String close;
+  final String change;
 
   Stock(
       {required this.symbol,
@@ -56,7 +52,7 @@ class Stock {
       required this.close,
       required this.change});
 
-  factory Stock.fromJson(Map<String, dynamic> json) {
+  factory Stock.fromJson(json) {
     return Stock(
         symbol: json["Symbol"],
         expdate: json["Ser/Exp"],
@@ -70,15 +66,9 @@ class Stock {
 
   static List<Stock> getAll() {
     List<Stock> stocks = <Stock>[];
-    stocks.add(Stock(
-        symbol: "Gold",
-        expdate: "03JAN2021",
-        ltp: 48130,
-        high: 48270,
-        low: 47976,
-        open: 48015,
-        close: 48018,
-        change: 213.67));
+    var json = getData('lol');
+    stocks.add(Stock.fromJson(json));
+
     return stocks;
   }
 }
